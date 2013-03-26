7f9a4284b4629ecf502c0bff4b04d32ca748ebee
Rahat Ahmed <firefreek@gmail.com>
2013-03-26 00:00:19 -0500

    Initial commit.

---
+++ src/com/freek/ledwidget/LEDService.java
@@ -0,0 +1,128 @@
+package com.freek.ledwidget;
+
+import java.io.IOException;
+
+import android.app.Service;
+import android.content.Intent;
+import android.graphics.SurfaceTexture;
+import android.hardware.Camera;
+import android.hardware.Camera.Parameters;
+import android.os.IBinder;
+import android.util.Log;
+import android.widget.Toast;
+
+public class LEDService extends Service
+{
+	public static final String ACTION_TOGGLE_LED = "com.freek.ledwidget.LEDWidgetProvider.ACTION_TOGGLE_LED";
+	boolean isLEDOn;
+	Camera camera;
+
+	@Override
+	public int onStartCommand(Intent intent, int flags, int startId)
+	{
+		isLEDOn = toggleLED();
+		return super.onStartCommand(intent, flags, startId);
+	}
+
+	@Override
+	public IBinder onBind(Intent intent)
+	{
+		return null;
+	}
+
+	@Override
+	public void onCreate()
+	{
+		super.onCreate();
+		isLEDOn = false;
+	}
+
+	/**
+	 * Toggles the LED
+	 * 
+	 * @return true if LED turned on, false if LED turned off
+	 */
+	private boolean toggleLED()
+	{
+		try
+		{
+
+			if (isLEDOn())
+			{
+				// Turn off the LED
+				turnLEDOff();
+				return false;
+			} else
+			{
+				// Turn on the LED
+				turnLEDOn();
+				return true;
+			}
+
+		} catch (Exception e)
+		{
+			toast("Unable to open camera.");
+			e.printStackTrace();
+		}
+		return false;
+	}
+
+	private void turnLEDOn() throws IOException
+	{
+		
+		// In order to work, the camera needs a surface to turn on.
+		// Here I pass it a dummy Surface Texture to make it happy.
+		try
+		{
+			camera = Camera.open();
+			camera.setPreviewTexture(new SurfaceTexture(0));
+			camera.startPreview();
+			Parameters p = camera.getParameters();
+			p.setFlashMode(Camera.Parameters.FLASH_MODE_TORCH);
+			camera.setParameters(p);
+			// Now update the widgets to reflect the change.
+			Intent intent = new Intent(this, LEDWidgetProvider.class);
+			intent.setAction(LEDWidgetProvider.UPDATE_WIDGET_ON);
+			sendBroadcast(intent);
+		} catch (Exception e) {
+			e.printStackTrace();
+			toast("Unable to turn on LED");
+		}
+		
+	}
+
+	private void turnLEDOff()
+	{
+		if (camera != null)
+		{
+			// Stopping the camera is enough to turn off the LED
+			camera.stopPreview();
+			camera.release();
+			camera = null;
+		} else
+			throw new NullPointerException("Camera doesn't exist to turn off.");
+		
+		// Now update the widgets to reflect the change.
+		Intent intent = new Intent(this, LEDWidgetProvider.class);
+		intent.setAction(LEDWidgetProvider.UPDATE_WIDGET_OFF);
+		sendBroadcast(intent);
+	}
+
+	boolean isLEDOn()
+	{
+		return camera != null;
+	}
+	
+	/**
+	 * Silly convenience method for making toasts.
+	 * @param msg
+	 */
+	private void toast(String msg)
+	{
+		Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
+	}
+	private void log(String msg)
+	{
+		Log.d("LED",msg);
+	}
+}
---
+++ src/com/freek/ledwidget/LEDWidgetProvider.java
@@ -0,0 +1,196 @@
+package com.freek.ledwidget;
+
+import android.annotation.TargetApi;
+import android.app.PendingIntent;
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.appwidget.AppWidgetProviderInfo;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.widget.RemoteViews;
+import android.widget.Toast;
+
+public class LEDWidgetProvider extends AppWidgetProvider
+{
+
+	public static final String UPDATE_WIDGET_ON = "com.freek.ledwidget.LEDWidgetProvider.UPDATE_WIDGET_ON";
+	public static final String UPDATE_WIDGET_OFF = "com.freek.ledwidget.LEDWidgetProvider.UPDATE_WIDGET_OFF";
+
+	@TargetApi(Build.VERSION_CODES.JELLY_BEAN_MR1)
+	@Override
+	public void onUpdate(Context context, AppWidgetManager appWidgetManager,
+			int[] appWidgetIds)
+	{
+		// Set pending intent to call the Service
+		Intent intent = new Intent(context, LEDService.class);
+		intent.setAction(LEDService.ACTION_TOGGLE_LED);
+		PendingIntent pIntent = PendingIntent.getService(context, 0, intent,
+				PendingIntent.FLAG_UPDATE_CURRENT);
+
+
+		for (int appWidgetId : appWidgetIds)
+		{
+			// Check what category of widget we're dealing with
+			// lockscreens only supported in 4.2+
+			boolean isLockscreen = false;
+			if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1)
+			{
+				Bundle options = appWidgetManager.getAppWidgetOptions(appWidgetId);
+				int category = options.getInt(AppWidgetManager.OPTION_APPWIDGET_HOST_CATEGORY, -1);
+				isLockscreen = category == AppWidgetProviderInfo.WIDGET_CATEGORY_KEYGUARD;	
+			}
+			
+			
+			if(isLockscreen) // Create lockscreen specific layout
+			{
+				// Create the layout and attach the listener
+				RemoteViews views = new RemoteViews(context.getPackageName(),
+						R.layout.led_widget_lockscreen_layout);
+				views.setOnClickPendingIntent(R.id.btn_led, pIntent);
+				// Set the image to off by default
+				// (This update should only be called on creation)
+				views.setImageViewResource(R.id.img_led,
+						R.drawable.appwidget_settings_led_off_big);
+				// Update the widget
+				appWidgetManager.updateAppWidget(appWidgetId, views);
+			}
+			else // Create homescreen specific layout
+			{
+				// Create the layout and attach the listener
+				RemoteViews views = new RemoteViews(context.getPackageName(),
+						R.layout.led_widget_layout);
+				views.setOnClickPendingIntent(R.id.btn_led, pIntent);
+				// Set the images to off by default
+				// (This update should only be called on creation)
+				views.setImageViewResource(R.id.img_led,
+						R.drawable.appwidget_settings_led_off);
+				views.setImageViewResource(R.id.ind_led,
+						R.drawable.appwidget_settings_ind_off_holo);
+				// Update the widget
+				appWidgetManager.updateAppWidget(appWidgetId, views);
+			}
+		}
+		
+
+	}
+
+	@TargetApi(Build.VERSION_CODES.JELLY_BEAN_MR1)
+	@Override
+	public void onReceive(Context context, Intent intent)
+	{
+		super.onReceive(context, intent);
+		// Here we do the same thing as onUpdate, except we know if
+		// the widget should use it's on images, or off images
+		if (intent.getAction().equals(UPDATE_WIDGET_OFF)
+				|| intent.getAction().equals(UPDATE_WIDGET_ON))
+		{
+
+			ComponentName thisWidget = new ComponentName(context,
+					LEDWidgetProvider.class);
+			AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(context);
+			int[] appWidgetIds = appWidgetManager.getAppWidgetIds(thisWidget);
+			
+//			// Set pending intent to call the Service
+			Intent widgetIntent = new Intent(context, LEDService.class);
+			widgetIntent.setAction(LEDService.ACTION_TOGGLE_LED);
+			PendingIntent pIntent = PendingIntent.getService(context, 0,
+					widgetIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+			
+			for (int appWidgetId : appWidgetIds)
+			{
+				// Check what category of widget we're dealing with
+				// lockscreens only supported in 4.2+
+				boolean isLockscreen = false;
+				if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1)
+				{
+					Bundle options = appWidgetManager.getAppWidgetOptions(appWidgetId);
+					int category = options.getInt(AppWidgetManager.OPTION_APPWIDGET_HOST_CATEGORY, -1);
+					isLockscreen = category == AppWidgetProviderInfo.WIDGET_CATEGORY_KEYGUARD;	
+				}
+				
+				
+				if(isLockscreen) // Create lockscreen specific layout
+				{
+					
+					// Create the layout and attach the listener
+					RemoteViews views = new RemoteViews(context.getPackageName(),
+							R.layout.led_widget_lockscreen_layout);
+					views.setOnClickPendingIntent(R.id.btn_led, pIntent);
+					
+					// Set the image to off by default
+					// (This update should only be called on creation)
+					if (intent.getAction().equals(UPDATE_WIDGET_ON))
+					{
+						views.setImageViewResource(R.id.img_led,
+								R.drawable.appwidget_settings_led_on_big);
+					} else if (intent.getAction().equals(UPDATE_WIDGET_OFF))
+					{
+						views.setImageViewResource(R.id.img_led,
+								R.drawable.appwidget_settings_led_off_big);
+					}
+					
+					// Update the widget
+					appWidgetManager.updateAppWidget(appWidgetId, views);
+				}
+				else // Create homescreen specific layout
+				{
+//					// Create the layout and the listener
+					RemoteViews views = new RemoteViews(context.getPackageName(),
+							R.layout.led_widget_layout);
+					views.setOnClickPendingIntent(R.id.btn_led, pIntent);
+//					// Set the images to the right setting
+					if (intent.getAction().equals(UPDATE_WIDGET_ON))
+					{
+						views.setImageViewResource(R.id.img_led,
+								R.drawable.appwidget_settings_led_on);
+						views.setImageViewResource(R.id.ind_led,
+								R.drawable.appwidget_settings_ind_on_holo);
+					} else if (intent.getAction().equals(UPDATE_WIDGET_OFF))
+					{
+						views.setImageViewResource(R.id.img_led,
+								R.drawable.appwidget_settings_led_off);
+						views.setImageViewResource(R.id.ind_led,
+								R.drawable.appwidget_settings_ind_off_holo);
+					}
+					appWidgetManager.updateAppWidget(appWidgetId, views);
+				}
+			}
+			
+
+//
+//			// Create the layout and the listener
+//			RemoteViews views = new RemoteViews(context.getPackageName(),
+//					R.layout.led_widget_layout);
+//			views.setOnClickPendingIntent(R.id.btn_led, pIntent);
+//			// Set the images to the right setting
+//			if (intent.getAction().equals(UPDATE_WIDGET_ON))
+//			{
+//				views.setImageViewResource(R.id.img_led,
+//						R.drawable.appwidget_settings_led_on);
+//				views.setImageViewResource(R.id.ind_led,
+//						R.drawable.appwidget_settings_ind_on_holo);
+//			} else if (intent.getAction().equals(UPDATE_WIDGET_OFF))
+//			{
+//				views.setImageViewResource(R.id.img_led,
+//						R.drawable.appwidget_settings_led_off);
+//				views.setImageViewResource(R.id.ind_led,
+//						R.drawable.appwidget_settings_ind_off_holo);
+//			}
+//			manager.updateAppWidget(thisWidget, views);
+		}
+
+	}
+
+	/**
+	 * Silly convenience method for making toasts.
+	 * 
+	 * @param msg
+	 */
+	private void toast(Context context, String msg)
+	{
+		Toast.makeText(context, msg, Toast.LENGTH_SHORT).show();
+	}
+}
