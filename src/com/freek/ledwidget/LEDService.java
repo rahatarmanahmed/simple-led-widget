59941b4a1e1c4b4c5928a38c69ee80f4893181e7
Rahat Ahmed <rahatarmanahmed@gmail.com>
2014-04-01 22:18:47 -0500

    Added partial wake lock to fix unresponsiveness issue.

--- src/com/freek/ledwidget/LEDService.java
+++ src/com/freek/ledwidget/LEDService.java
@@ -4,12 +4,14 @@ import java.io.IOException;
 
 import android.annotation.SuppressLint;
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.SurfaceTexture;
 import android.hardware.Camera;
 import android.hardware.Camera.Parameters;
-import android.hardware.Camera.PreviewCallback;
 import android.os.IBinder;
+import android.os.PowerManager;
+import android.os.PowerManager.WakeLock;
 import android.util.Log;
 import android.widget.Toast;
 
@@ -19,6 +21,7 @@ public class LEDService extends Service
 	boolean isLEDOn;
 	Camera camera;
 	SurfaceTexture surfaceTexture;
+	WakeLock wakeLock;
 
 	@Override
 	public int onStartCommand(Intent intent, int flags, int startId)
@@ -81,50 +84,70 @@ public class LEDService extends Service
 
 	private void turnLEDOn() throws IOException
 	{
-		
-		// In order to work, the camera needs a surface to turn on.
-		// Here I pass it a dummy Surface Texture to make it happy.
+		Log.d("LED", "Beginning turnLEDOn()");
 		try
 		{
-			Log.d("LED", "beginning turnLEDOn()");
-			Log.d("LED", "opening camera");
+			// Acquire wakelock to fix unresponsiveness after locking
+			Log.d("LED", "Acquiring wake lock");
+			PowerManager pm = (PowerManager) getSystemService(Context.POWER_SERVICE);
+			if(wakeLock != null && wakeLock.isHeld()) // Shouldn't need this but never hurts.
+				wakeLock.release();
+			wakeLock = pm.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK, "LED");
+			wakeLock.acquire();
+			Log.d("LED", "Wake lock acquired");
+			
+			Log.d("LED", "Opening camera");
 			camera = Camera.open();
 			
-			Log.d("LED", "setting parameters");
+			Log.d("LED", "Setting parameters");
 			Parameters p = camera.getParameters();
 			p.setFlashMode(Camera.Parameters.FLASH_MODE_TORCH);
 			camera.setParameters(p);
 			
-			Log.d("LED", "starting preview");
+			Log.d("LED", "Starting preview");
 			camera.startPreview();
 
-			Log.d("LED", "setting preview texture");
+			// In order to work, the camera needs a surface to turn on.
+			// Here I pass it a dummy Surface Texture to make it happy.
+			Log.d("LED", "Setting preview texture");
 			surfaceTexture = new SurfaceTexture(0);
 			camera.setPreviewTexture(surfaceTexture);
 			
-			Log.d("LED", "updating widgets");
+			Log.d("LED", "Updating widgets");
 			// Now update the widgets to reflect the change.
 			Intent intent = new Intent(this, LEDWidgetProvider.class);
 			intent.setAction(LEDWidgetProvider.UPDATE_WIDGET_ON);
 			sendBroadcast(intent);
 			
-			Log.d("LED", "ending turnLEDOn()");
 		} catch (Exception e) {
 			e.printStackTrace();
 			toast("Unable to turn on LED");
 		}
-		
+		Log.d("LED", "Ending turnLEDOn()");		
 	}
 
 	@SuppressLint("NewApi")
 	private void turnLEDOff()
 	{
+		Log.d("LED", "Beginning turnLEDOff()");
+		
+		if (wakeLock != null)
+		{
+			Log.d("LED", "Releasing wake lock");
+			wakeLock.release();
+			wakeLock = null;
+			Log.d("LED", "Wake lock released");
+		}
+		
 		if (camera != null)
 		{
 			try
 			{
 				// Stopping the camera is enough to turn off the LED
+				Log.d("LED", "Camera isn't null, stopping it to turn off LED.");
+				Log.d("LED", "Stopping preview.");
 				camera.stopPreview();
+				Log.d("LED", "Releasing camera.");
 				camera.release();
 				camera = null;
 			} catch (Exception e) {
@@ -134,9 +157,11 @@ public class LEDService extends Service
 			throw new NullPointerException("Camera doesn't exist to turn off.");
 		
 		// Now update the widgets to reflect the change.
+		Log.d("LED", "Updating widgets");
 		Intent intent = new Intent(this, LEDWidgetProvider.class);
 		intent.setAction(LEDWidgetProvider.UPDATE_WIDGET_OFF);
 		sendBroadcast(intent);
+		Log.d("LED", "Beginning turnLEDOff()");
 	}
 
 	boolean isLEDOn()
