fb605800aa587c14b1acb29987d0df141125a0e1
Rahat Ahmed <rahatarmanahmed@gmail.com>
2014-07-06 00:29:10 -0500

    Added notification to turn off LED when on.

--- src/com/freek/ledwidget/LEDService.java
+++ src/com/freek/ledwidget/LEDService.java
@@ -3,6 +3,9 @@ package com.freek.ledwidget;
 import java.io.IOException;
 
 import android.annotation.SuppressLint;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
@@ -12,12 +15,14 @@ import android.hardware.Camera.Parameters;
 import android.os.IBinder;
 import android.os.PowerManager;
 import android.os.PowerManager.WakeLock;
+import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 import android.widget.Toast;
 
 public class LEDService extends Service
 {
-	public static final String ACTION_TOGGLE_LED = "com.freek.ledwidget.LEDWidgetProvider.ACTION_TOGGLE_LED";
+	public static final String ACTION_TOGGLE_LED = "com.freek.ledwidget.LEDService.ACTION_TOGGLE_LED";
+	public static final int NOTIFICATION_ID = 1;
 	boolean isLEDOn;
 	Camera camera;
 	SurfaceTexture surfaceTexture;
@@ -26,7 +31,9 @@ public class LEDService extends Service
 	@Override
 	public int onStartCommand(Intent intent, int flags, int startId)
 	{
-		if(intent.getAction().equals(ACTION_TOGGLE_LED))
+		Log.d("LED", intent.toString());
+		Log.d("LED", "Action: " + intent.getAction());
+		if(ACTION_TOGGLE_LED.equals(intent.getAction()))
 			isLEDOn = toggleLED();
 		return Service.START_NOT_STICKY;
 	}
@@ -120,6 +127,24 @@ public class LEDService extends Service
 			intent.setAction(LEDWidgetProvider.UPDATE_WIDGET_ON);
 			sendBroadcast(intent);
 			
+			
+			Intent notificationIntent = new Intent(this, LEDService.class);
+			notificationIntent.setAction(ACTION_TOGGLE_LED);
+			
+			PendingIntent pendingIntent = PendingIntent.getService(this, 0, notificationIntent,
+					PendingIntent.FLAG_UPDATE_CURRENT);			
+			
+			NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+			Notification notification = new NotificationCompat.Builder(this)
+				.setSmallIcon(R.drawable.appwidget_settings_led_on)
+				.setContentTitle("LED is ON")
+				.setContentText("Tap to turn off.")
+				.setPriority(NotificationCompat.PRIORITY_HIGH)
+				.setOngoing(true)
+				.setContentIntent(pendingIntent)
+				.build();
+			notificationManager.notify(NOTIFICATION_ID, notification);
+			
 		} catch (Exception e) {
 			e.printStackTrace();
 			toast("Unable to turn on LED");
@@ -163,6 +188,9 @@ public class LEDService extends Service
 		intent.setAction(LEDWidgetProvider.UPDATE_WIDGET_OFF);
 		sendBroadcast(intent);
 		Log.d("LED", "Beginning turnLEDOff()");
+		
+		NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+		notificationManager.cancel(NOTIFICATION_ID);
 	}
 
 	boolean isLEDOn()
